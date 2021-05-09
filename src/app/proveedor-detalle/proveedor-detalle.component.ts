eeb51b94182dad66c6024aa2bae410fb31476769
Nazir Rosas <a2116018@correo.uia.mx>
2021-05-08 20:33:52 -0500

    1 commit

---
+++ src/app/proveedor-detalle/proveedor-detalle.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core'
+import { Proveedor } from '../proveedor';
+import { ProveedorService } from '../proveedor.service';
+
+@Component({
+  selector: 'app-proveedor-detalle',
+  templateUrl: './proveedor-detalle.component.html',
+  styleUrls: ['./proveedor-detalle.component.css']
+})
+export class ProveedorDetalleComponent implements OnInit {
+
+  @Input() proveedor?: Proveedor;
+  
+  constructor( private datosProveedor:ProveedorService) { }
+
+  ngOnInit(): void {
+  }
+
+  actualizar ()
+  {
+    console.log(this.proveedor.name);
+    console.log(this.proveedor.id);
+
+    var newProveedor = <Proveedor>{};
+
+    newProveedor.id=this.proveedor.id;
+    newProveedor.name=this.proveedor.name;
+    newProveedor.type="proveedor";
+    newProveedor.saldo=0;
+    newProveedor.estado= "nuevo";
+  }
+
+}
